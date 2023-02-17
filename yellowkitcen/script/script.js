1ad7cc47d452980d12fcbf05ac99408b8bd5fd2e
Yurii <105108355+dizer2@users.noreply.github.com>
2023-02-17 19:57:52 +0100

    Add files via upload

---
+++ yellowkitcen/script/script.js
@@ -0,0 +1,271 @@
+var menu = document.getElementById("menu");
+var nav1 = document.getElementById("delivery");
+var nav2 = document.getElementById("deliver2");
+var nav3 = document.getElementById("delivery3");
+var nav4 = document.getElementById("delivery4");
+var main__btn = document.getElementById("main__btn");
+var main__btn2 = document.getElementById("main__btn2");
+var paje3__btn = document.getElementById("paje3__btn");
+var paje4__btn = document.getElementById("paje4__btn");
+var paje5__btn = document.getElementById("paje5__btn");
+
+// burger 
+menu.onclick = function() {
+	burger__active.classList.toggle("burger__activegggg");
+	paje1.classList.toggle("body__burger");
+	body.classList.toggle("bodyy");
+	paje1.classList.toggle("ggg");
+	paje1.classList.toggle("main__burger");
+	header__burger.classList.toggle("header__burger");
+	main.classList.toggle("main__noned");
+	paje2.classList.toggle("hhhhhhh");
+	paje3.classList.toggle("hhhhhhh");
+	main__left.classList.toggle("main__none");
+	main__right.classList.toggle("main__noned");
+	paje4.classList.toggle("main__noned");
+	burger__leftrow.classList.toggle("burger__row__left");
+	burger__rightrow.classList.toggle("burger__row__right");
+	burger__rownone.classList.toggle("burger-rownone");
+	paje5.classList.toggle("main__noned");
+	paje6.classList.toggle("main__noned");
+	paje7.classList.toggle("main__noned");
+	paje8.classList.toggle("main__noned");
+	paje9.classList.toggle("main__noned");
+
+}
+nav1.onclick = function() {
+	paje2.classList.remove("hhhhhhh");
+	paje3.classList.remove("hhhhhhh");
+	paje2.classList.remove("main__noned");
+	paje4.classList.remove("main__noned");
+	paje4.scrollIntoView({
+		block: "center",
+		inline: "nearest",
+		behavior: "smooth"
+	});
+	burger__active.classList.remove("burger__activegggg");
+	paje1.classList.remove("body__burger");
+	paje1.classList.remove("ggg");
+	header__burger.classList.remove("header__burger");
+	main.classList.remove("main__noned");7
+	paje1.classList.toggle("main__burger");
+	paje2.classList.remove("main__noned");
+	main__left.classList.remove("main__none");
+	main__right.classList.remove("main__noned");
+	body.classList.remove("bodyy");
+	burger__leftrow.classList.remove("burger__row__left");
+	burger__rightrow.classList.remove("burger__row__right");
+	burger__rownone.classList.remove("burger-rownone");
+	paje5.classList.remove("main__noned");
+	paje6.classList.remove("main__noned");
+	paje7.classList.remove("main__noned");
+	paje8.classList.remove("main__noned");
+	paje9.classList.remove("main__noned");
+}
+nav2.onclick = function() {
+	paje5.classList.remove("main__noned");
+	paje6.classList.remove("main__noned");
+	paje7.classList.remove("main__noned");
+	paje8.classList.remove("main__noned");
+	paje9.classList.remove("main__noned");
+	paje2.classList.remove("hhhhhhh");
+	paje3.classList.remove("hhhhhhh");
+	paje2.classList.remove("main__noned");
+	paje4.classList.remove("main__noned");
+	paje8.scrollIntoView({
+		block: "center",
+		inline: "nearest",
+		behavior: "smooth"
+	});
+	burger__active.classList.remove("burger__activegggg");
+	paje1.classList.remove("body__burger");
+	paje1.classList.remove("ggg");
+	header__burger.classList.remove("header__burger");
+	main.classList.remove("main__noned");7
+	paje1.classList.toggle("main__burger");
+	paje2.classList.remove("main__noned");
+	main__left.classList.remove("main__none");
+	main__right.classList.remove("main__noned");
+	body.classList.remove("bodyy");
+	burger__leftrow.classList.remove("burger__row__left");
+	burger__rightrow.classList.remove("burger__row__right");
+	burger__rownone.classList.remove("burger-rownone");
+	paje5.classList.remove("main__noned");
+	paje6.classList.remove("main__noned");
+	paje7.classList.remove("main__noned");
+	paje8.classList.remove("main__noned");
+	paje9.classList.remove("main__noned");
+}
+nav3.onclick = function() {
+	paje5.classList.remove("main__noned");
+	paje6.classList.remove("main__noned");
+	paje7.classList.remove("main__noned");
+	paje8.classList.remove("main__noned");
+	paje9.classList.remove("main__noned");
+	paje2.classList.remove("hhhhhhh");
+	paje3.classList.remove("hhhhhhh");
+	paje2.classList.remove("main__noned");
+	paje4.classList.remove("main__noned");
+	paje3.scrollIntoView({
+		block: "center",
+		inline: "nearest",
+		behavior: "smooth"
+	});
+	burger__active.classList.remove("burger__activegggg");
+	paje1.classList.remove("body__burger");
+	paje1.classList.remove("ggg");
+	header__burger.classList.remove("header__burger");
+	main.classList.remove("main__noned");7
+	paje1.classList.toggle("main__burger");
+	paje2.classList.remove("main__noned");
+	main__left.classList.remove("main__none");
+	main__right.classList.remove("main__noned");
+	body.classList.remove("bodyy");
+	burger__leftrow.classList.remove("burger__row__left");
+	burger__rightrow.classList.remove("burger__row__right");
+	burger__rownone.classList.remove("burger-rownone");
+	paje5.classList.remove("main__noned");
+	paje6.classList.remove("main__noned");
+	paje7.classList.remove("main__noned");
+	paje8.classList.remove("main__noned");
+	paje9.classList.remove("main__noned");
+
+}
+nav4.onclick = function() {
+	paje5.classList.remove("main__noned");
+	paje6.classList.remove("main__noned");
+	paje7.classList.remove("main__noned");
+	paje8.classList.remove("main__noned");
+	paje9.classList.remove("main__noned");
+	paje2.classList.remove("hhhhhhh");
+	paje3.classList.remove("hhhhhhh");
+	paje2.classList.remove("main__noned");
+	paje4.classList.remove("main__noned");
+	paje5.scrollIntoView({
+		block: "center",
+		inline: "nearest",
+		behavior: "smooth"
+	});
+	burger__active.classList.remove("burger__activegggg");
+	paje1.classList.remove("body__burger");
+	paje1.classList.remove("ggg");
+	header__burger.classList.remove("header__burger");
+	main.classList.remove("main__noned");7
+	paje1.classList.toggle("main__burger");
+	paje2.classList.remove("main__noned");
+	main__left.classList.remove("main__none");
+	main__right.classList.remove("main__noned");
+	body.classList.remove("bodyy");
+	burger__leftrow.classList.remove("burger__row__left");
+	burger__rightrow.classList.remove("burger__row__right");
+	burger__rownone.classList.remove("burger-rownone");
+
+
+}
+
+// btn hover and activ ⬇️
+
+main__btn.onmouseenter = function () {
+	main__btn.classList.remove("main__btn");
+	main__btn.classList.toggle("btn__texhover");
+	main__btn.onmousedown = function showHover(num){
+		main__btn.classList.toggle("btn__activImg");
+	}
+	main__btn.onmouseleave = function () {
+		main__btn.classList.toggle("main__btn");
+		main__btn.classList.toggle("btn__texhover");
+		main__btn.classList.remove("btn__activImg");
+	}
+}
+main__btn2.onmouseenter = function () {
+	main__btn2.classList.remove("main__btn");
+	main__btn2.classList.toggle("btn__texhover");
+	main__btn2.onmousedown = function () {
+		main__btn2.classList.toggle("btn__activImg");
+	}
+	main__btn2.onmouseleave = function () {
+		main__btn2.classList.toggle("main__btn");
+		main__btn2.classList.toggle("btn__texhover");
+		main__btn2.classList.remove("btn__activImg");
+	
+	}
+}
+paje3__btn.onmouseenter = function () {
+	paje3__btn.classList.remove("paje3__btn");
+	paje3__btn.classList.toggle("paje2btn__texhover");
+	paje3__btn.onmousedown = function () {
+		paje3__btn.classList.toggle("paje2btn__activImg");
+	}
+	paje3__btn.onmouseleave = function () {
+		paje3__btn.classList.toggle("paje3__btn");
+		paje3__btn.classList.toggle("paje2btn__texhover");
+		paje3__btn.classList.remove("paje2btn__activImg");
+	}
+}
+paje4__btn.onmouseenter = function () {
+	paje4__btn.classList.remove("paje4main__btn");
+	paje4__btn.classList.toggle("paje4btn__texhover");
+	paje4__btn.onmousedown = function () {
+		loader.classList.toggle("loafer__active");
+		paje4__text.classList.toggle("main__noned");
+	}
+	paje4__btn.onmouseleave = function () {
+		paje4__btn.classList.toggle("paje4main__btn");
+		paje4__btn.classList.toggle("paje4btn__texhover");
+		paje4__btn.classList.remove("paje4btn__activImg");
+		paje4__text.classList.remove("main__noned");
+		loader.classList.remove("loafer__active");
+	}
+}
+paje5__btn.onmouseenter = function () {
+	paje5__btn.classList.remove("paje5__btn");
+	paje5__btn.classList.toggle("paje5btn__texhover");
+	paje5__btn.onmousedown = function () {
+		paje5__btn.classList.toggle("paje5btn__activImg");
+	}
+	paje5__btn.onmouseleave = function () {
+		paje5__btn.classList.toggle("paje5__btn");
+		paje5__btn.classList.toggle("paje5btn__texhover");
+		paje5__btn.classList.remove("paje5btn__activImg");
+	}
+}
+
+
+function onEntry(entry){
+	entry.forEach(change =>  {
+		if(change.isIntersecting) {
+			change.target.classList.add("paje3__show")
+		}
+	});
+}
+let options = {
+	threshold: [0.5]};
+
+let observer = new IntersectionObserver(onEntry, options);
+let observer2 = new IntersectionObserver(onEntry, options);
+
+
+let elements = document.querySelectorAll(".paje4")
+let elements4 = document.querySelectorAll(".paje5")
+let elements5 = document.querySelectorAll(".paje6")
+let elements6 = document.querySelectorAll(".paje7")
+let elements7 = document.querySelectorAll(".paje8")
+
+
+for(let elm of elements) {
+	observer.observe(elm);
+}
+for(let elm2 of elements4) {
+	observer2.observe(elm2);
+}
+for(let elm2 of elements5) {
+	observer2.observe(elm2);
+}
+for(let elm2 of elements6) {
+	observer2.observe(elm2);
+}
+for(let elm2 of elements7) {
+	observer2.observe(elm2);
+}
+
+
